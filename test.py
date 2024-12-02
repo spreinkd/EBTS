36f7bb5c8513f32978a94ee6aa088587d5f57894
spreinkd <94915326+spreinkd@users.noreply.github.com>
2024-12-02 10:18:30 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,14 @@
-CASE WHEN (CASE 
-    WHEN CHARINDEX(', ', QuoteConfiguration.[Name]) > 0 AND CHARINDEX(' Days', QuoteConfiguration.[Name]) > CHARINDEX(', ', QuoteConfiguration.[Name])
-    THEN CAST(
-        TRIM(SUBSTRING(
+DATEADD(
+    DAY,
+    COALESCE(
+        TRY_CAST(
+            TRIM(SUBSTRING(
                 QuoteConfiguration.[Name], 
                 CHARINDEX(', ', QuoteConfiguration.[Name]) + 2, 
-                CHARINDEX(' days', QuoteConfiguration.[Name]) - CHARINDEX(', ', QuoteConfiguration.[Name]) - 2
-            )
-        ) AS INT
-    )
-    ELSE NULL
-END)is NULL  THEN DATEADD(DAY,30,CustomerQuote.EffectiveDate)
-ELSE DATEADD(DAY,    (CASE 
-    WHEN CHARINDEX(', ', QuoteConfiguration.[Name]) > 0 AND CHARINDEX(' Days', QuoteConfiguration.[Name]) > CHARINDEX(', ', QuoteConfiguration.[Name])
-    THEN CAST(
-        TRIM(
-            SUBSTRING(
-                QuoteConfiguration.[Name], 
-                CHARINDEX(', ', QuoteConfiguration.[Name]) + 2, 
-                CHARINDEX(' days', QuoteConfiguration.[Name]) - CHARINDEX(', ', QuoteConfiguration.[Name]) - 2
-            )
-        ) AS INT
-    )
-    ELSE NULL
-END),CustomerQuote.EffectiveDate) 
+                CHARINDEX(' Days', QuoteConfiguration.[Name]) - CHARINDEX(', ', QuoteConfiguration.[Name]) - 2
+            )) AS INT
+        ),
+        30
+    ),
+    CustomerQuote.EffectiveDate
+)
