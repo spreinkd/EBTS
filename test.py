d23ab6a1518d827354f9d81577358ee5d4d2bd1c
spreinkd <94915326+spreinkd@users.noreply.github.com>
2024-12-04 12:59:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,18 @@
-import requests
-
-# Replace with your details
-auth_url = "A"
-token_url = "B" 
-client_id = "C" 
-client_secret = "D"
-
 # Step 1: Obtain Access Token
-data={
-        "grant_type": "client_credentials",
-        "client_id": client_id,
-        "client_secret": client_secret,
-    }
-headers={
+data = {
+    "grant_type": "client_credentials",
+    "client_id": client_id,
+    "client_secret": client_secret,
+}
+
+headers = {
     "Accept": "application/json",
     "Accept-Language": "en-US",
     "Content-Type": "application/json",
 }
-response = requests.post( token_url,data=data,headers=headers)
-response
 
-<Response [400]>
+response = requests.post(token_url, json=data, headers=headers)
+
+# Check the response status and content
+print(response.status_code)
+print(response.text)
